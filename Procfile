057d5ff5edb02092e5d76cd2e04e0c18b8f4688d
Zach Tibbitts <zachtib@gmail.com>
2021-02-15 13:03:53 -0500

    fix uvicorn configuration

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: uvicorn app.asgi:app --host=0.0.0.0 --port=$PORT
+web: uvicorn app.asgi:application --host=0.0.0.0 --port=$PORT
--- app/asgi.py
+++ app/asgi.py
@@ -8,14 +8,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
 import django
+
+# This needs to run before the following imports: https://www.gitmemory.com/issue/django/channels/1564/722354397
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
