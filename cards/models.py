770ea4315164bb5b32e6f7e5db870ff8bcb8bbd5
Zach Tibbitts <zachtib@gmail.com>
2021-05-20 09:39:29 -0400

    update importing to handle DFCs

--- cards/models.py
+++ cards/models.py
@@ -1,10 +1,10 @@
 import uuid
 
-from django.db import models
+from django.db import models, transaction
 from django.utils.translation import gettext_lazy as _
 
 from scryfall.client import ScryfallClient
-from scryfall.models import ScryfallCard
+from scryfall.models import ScryfallCard, ScryfallCardFace
 
 
 class Color(models.TextChoices):
@@ -45,12 +45,34 @@ class CardManager(models.Manager):
         super().__init__()
         self.scryfall = ScryfallClient()
 
-    def from_scryfall_card(self, card: ScryfallCard) -> 'Card':
-        return Card.objects.create(
-            id=card.id,
-            name=card.name,
-            mana_cost=card.mana_cost
+    def from_scryfall_cardface(self, parent_card, index, scryfall_face: ScryfallCardFace) -> 'CardFace':
+        card_face = CardFace(
+            card=parent_card,
+            index=index,
+            name=scryfall_face.name,
+            mana_cost=scryfall_face.mana_cost
         )
+        return card_face
+
+    def from_scryfall_card(self, scryfall_card: ScryfallCard) -> 'Card':
+        is_dfc = scryfall_card.card_faces is not None and len(scryfall_card.card_faces) > 1
+        new_card = Card(id=scryfall_card.id, name=scryfall_card.name)
+        faces = []
+        if is_dfc:
+            mana_costs = []
+            for index, face in enumerate(scryfall_card.card_faces):
+                if face.mana_cost is not None and face.mana_cost != '':
+                    mana_costs.append(face.mana_cost)
+                faces.append(self.from_scryfall_cardface(new_card, index, face))
+            new_card.mana_cost = ' // '.join(mana_costs)
+        else:
+            new_card.mana_cost = scryfall_card.mana_cost
+        with transaction.atomic():
+            new_card.save()
+            if is_dfc:
+                for face in faces:
+                    face.save()
+        return new_card
 
     def get_or_fetch_printing_for_name(self, name: str):
         queryset = self.get_queryset()
@@ -62,6 +84,8 @@ class CardManager(models.Manager):
         except Card.DoesNotExist:
             card = None
         #  Card doesn't exist in our database, let's fetch it
+        #  Handle DFCs
+        name, _ = name.split('/', maxsplit=1)
         scryfall_card = self.scryfall.get_card_by_name_fuzzy(name)
         if card is None:
             card = self.from_scryfall_card(scryfall_card)
@@ -71,10 +95,15 @@ class CardManager(models.Manager):
             'name': scryfall_card.set_name,
             'id': set_id,
         })
+        if scryfall_card.card_faces is not None and len(scryfall_card.card_faces) > 1:
+            face = scryfall_card.card_faces[0]
+            image_url = face.image_uris.normal
+        else:
+            image_url = scryfall_card.image_uris.normal
         printing = Printing.objects.create(
             magic_set=magic_set,
             card=card,
-            image_url=scryfall_card.image_uris.normal
+            image_url=image_url
         )
         return printing
 
@@ -124,6 +153,7 @@ class MagicSet(models.Model):
 
 class CardFace(models.Model):
     card = models.ForeignKey(Card, related_name='faces', on_delete=models.CASCADE)
+    index = models.IntegerField(default=0)
     name = models.CharField(max_length=200)
     mana_cost = models.CharField(max_length=20, blank=True, null=True)
 
