d0b7b809b7967f649e7b5eb51c9cee6eba451d4b
Zach Tibbitts <zachtib@gmail.com>
2021-05-23 11:51:41 -0400

    Check for DEBUG in production

--- app/settings.py
+++ app/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'development_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = []
 
